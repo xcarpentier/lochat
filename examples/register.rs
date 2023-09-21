0d0f2cde86918f6da99535cdc7ab460d149b74e2
Xavier Carpentier <xcapetir@gmail.com>
2023-09-21 17:33:59 +0200

    local chat

--- examples/register.rs
+++ examples/register.rs
@@ -2,7 +2,7 @@
 async fn main() -> std::io::Result<()> {
 	// Use `cargo run --example register`
 
-	let (_registration, result) = async_dnssd::register("_ssh._tcp", 2022)?.await?;
+	let (_registration, result) = async_dnssd::register("_chat._tcp", 2022)?.await?;
 	println!("Registered: {:?}", result);
 
 	// wait until killed
